fdc2944416585e953cabb7243b6e29ee243eccd1
Aleksandr <akovalenko881@gmail.com>
2024-07-26 21:52:34 +0200

    done

--- HomeWork_07/decor.py
+++ HomeWork_07/decor.py
@@ -13,18 +13,15 @@ def input_error(expected_arg_count=None):
                 # checking arguments availability
                 if not args or not args[0]:
                     raise IndexError(error_messages["IndexError"])
-                
                 # checking numbers og arguments
                 if expected_arg_count is not None and len(args[0]) != expected_arg_count:
                     raise ValueError(error_messages["ValueError"])
-                
                 # checking additionaly some spesific functions
                 if func.__name__ in ["add_contact", "change_contact"]:
                     if not args[0][0].isalpha():
                         raise ValueError(error_messages["InvalidName"])
                     if not args[0][1].isdigit():
                         raise ValueError(error_messages["InvalidPhone"])
-                
                 return func(*args, **kwargs)
             except Exception as e:
                 return str(e)
